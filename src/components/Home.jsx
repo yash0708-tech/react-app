15d041394dac7ec5cb6e2afd64f400bc4fc9a37a
yash0708-tech <yash.b@technmanconsulting.com>
2024-03-15 08:49:10 -0400

    6 Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ const currentSession = async () => {
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#">Student Managementttt</a>
+        <a className="navbar-brand" href="#">Student Managementttttt</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
